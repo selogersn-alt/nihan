ffbd285e4d707b845cb14ab2b263a13188e18b5a
selogersn@gmail.com <selogersn@gmail.com>
2026-05-28 16:24:46 +0000

    fix: ajout redisUrl et sleep pour postgres, lien symbolique pnpm

--- apps/storefront/src/lib/config.ts
+++ apps/storefront/src/lib/config.ts
@@ -8,6 +8,10 @@ if (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL) {
   MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
 }
 
+if (process.env.MEDUSA_BACKEND_URL) {
+  MEDUSA_BACKEND_URL = process.env.MEDUSA_BACKEND_URL
+}
+
 export const sdk = new Medusa({
   baseUrl: MEDUSA_BACKEND_URL,
   debug: process.env.NODE_ENV === "development",
